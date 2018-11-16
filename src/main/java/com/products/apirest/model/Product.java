55d3598ecd8c48f1961a5a4833cd10bf9290dc24
augusto.klein <augusto.klein@itsgroup.com.br>
2018-11-16 16:12:12 -0200

    make it better

---
+++ src/main/java/com/products/apirest/model/Product.java
@@ -0,0 +1,65 @@
+package com.products.apirest.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import io.swagger.annotations.Api;
+
+import javax.persistence.GenerationType;
+@Entity
+@Table(name = "PRODUCT")
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	
+	private BigDecimal quantity;
+	
+	private BigDecimal price;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	
+}
---
+++ src/main/java/com/products/apirest/resources/ProductResource.java
@@ -0,0 +1,60 @@
+package com.products.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.products.apirest.model.Product;
+import com.products.apirest.repository.ProductRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping(value="/api")
+@Api(value="API REST Products")
+@CrossOrigin(origins="*")
+public class ProductResource {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	@GetMapping("/products")
+	@ApiOperation(value = "Returns the list of products")
+	public List<Product> productList(){
+		return productRepository.findAll();
+	}
+	
+	@GetMapping("/products({id})")
+	@ApiOperation(value = "Returns a single product")
+	public Product productEntry(@PathVariable(value="id") long id){
+		return productRepository.findById(id);
+	}
+	
+	@PostMapping("/products")
+	@ApiOperation(value = "Create a product")
+	public Product saveProduct(@RequestBody Product product){
+		return productRepository.save(product);
+	} 
+	
+	@DeleteMapping("/products({id})")
+	@ApiOperation(value = "Delete a product")
+	public void deleteProduct(@PathVariable(value="id") long id){
+		productRepository.delete(productRepository.findById(id));
+	}
+	
+	@PutMapping("/products")
+	@ApiOperation(value = "Update a product")
+	public Product updateProduct(@RequestBody Product product){
+		return productRepository.save(product);	
+	}
+}
